c7e84e36bed4691d3ba068382a4a3fda38a4b0ed
johnmaxnew <forevermeticulous@gmail.com>
2021-10-08 03:12:12 +0300

    initial commit

---
+++ sampleapp/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.sampleapp_index, name="sampleapp_index"),
+    path('<slug>/', views.sampleapp_detail, name="sampleapp_detail"),
+]
+
+
+
+
+
--- sampleapp/views.py
+++ sampleapp/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Post
+from django.core.paginator import Paginator
 
-# Create your views here.
+
+def sampleapp_index(request):
+    posts = Post.objects.all().order_by('-created_on')
+    most_recent = Post.objects.order_by('-created_on')[:5]
+    
+    
+    paginator = Paginator(posts, 5) # Show 25 contacts per page
+    page = request.GET.get('page')
+    posts = paginator.get_page(page)
+
+    context = {
+        "posts": posts,
+        'most_recent': most_recent,
+    }
+    return render(request, "sampleapp_index.html", context)
+
+
+def sampleapp_detail(request, slug):
+    post = Post.objects.get(slug=slug)
+
+
+    context = {
+        "post": post,
+    }
+    return render(request, "sampleapp_detail.html", context)
+
+
+def error_404(request, exception):
+    return render(request, "404.html")
